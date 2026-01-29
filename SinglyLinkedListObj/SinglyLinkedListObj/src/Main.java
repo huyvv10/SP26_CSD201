5eb160c722d195eb48cce4596474eaf8a77281cb
Vu Van Huy <huyvv10@fpt.edu.vn>
2026-01-29 10:29:49 +0700

    commit

--- SinglyLinkedListObj/SinglyLinkedListObj/src/Main.java
+++ SinglyLinkedListObj/SinglyLinkedListObj/src/Main.java
@@ -1,4 +1,7 @@
 
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,5 +12,29 @@ public class Main {
         myList.addFirst(new Car("V07", "Vinfast VF7", 2, 35000.8));
         myList.addFirst(new Car("V09", "Vinfast VF3", 4, 21000.2));
         myList.display();
+        myList.addLast(new Car("F01", "Maybach", 3, 150000));
+        myList.addLast(new Car("F02", "Fantom ghost", 6, 2000000));
+        myList.display();
+        System.out.println("Number of cars: "+ myList
+        .countCars());
+        System.out.print("Input color to count: ");
+        Scanner sn = new Scanner(System.in);
+        int xColor = sn.nextInt();
+        System.out.println("Number of cars with color as "+ 
+                xColor+ " is: " +myList.countCarsByColor(xColor));
+        sn.nextLine();  //Clear buffer
+        System.out.print("Input car name to search: ");
+        String xName = sn.nextLine();
+        myList.searchCarsByName(xName);
+        System.out.print("Input car Id to modify its info: ");
+        String xId;
+        xId = sn.nextLine();
+        myList.editCarById(xId);
+        myList.display();
+        System.out.print("Input car Id to remove: ");
+        xId = sn.nextLine();  
+        myList.removeCarById(xId);
+        myList.display();
+        
     }
 }
--- SinglyLinkedListObj/SinglyLinkedListObj/src/SinglyLinkedListObjMng.java
+++ SinglyLinkedListObj/SinglyLinkedListObj/src/SinglyLinkedListObjMng.java
@@ -1,4 +1,7 @@
 
+import java.util.Scanner;
+
+
 public class SinglyLinkedListObjMng {
 
     Node head, tail;
@@ -26,32 +29,147 @@ public class SinglyLinkedListObjMng {
     //If name is containing "fan" then do nothing,
     //add to last of the list otherwise, ignore case sensitivity
     public void addLast(Car x){
-        
+        Node newNode = new Node(x);
+        if (x.getName().toLowerCase().contains("fan")) return;
+        if (isEmpty()){
+            head=tail=newNode;
+            return;
+        }
+        tail.next=newNode;
+        tail=newNode;
     }
     
     //Count the number of cars in the list
     public int countCars(){
-        return 0;
+        int count=0;
+        Node cur = head;
+        while (cur!=null){
+            count++;
+            cur=cur.next;
+        }
+        return count;
     }
     
     //Count number of cars by their color
     public int countCarsByColor(int xColor){
-        return 0;
+        int count=0;
+        Node cur = head;
+        while (cur!=null){
+            if (cur.info.getColor()==xColor) {
+                count++;
+            }
+            cur=cur.next;
+        }
+        return count;
     }
     
     //Search cars by their name
     public void searchCarsByName(String xName){
-        
+        boolean flag=false;
+        Node cur = head;
+        while (cur!=null){
+            if (cur.info.getName().toLowerCase()
+                    .contains(xName.toLowerCase())){
+                flag=true;
+                System.out.println(cur.info.toString());
+            }
+            cur=cur.next;
+        }
+        if (!flag) {
+            System.out.println("Find not fount any cars.");
+        }
     }
     
     //Update car info by its ID
     public void editCarById(String xId){
-        
+        Scanner sn = new Scanner(System.in);
+        boolean found = false;
+        Node cur = head;
+        while (cur!=null){
+            if (cur.info.getId().equals(xId)){
+                System.out.print("Input new name: ");
+                cur.info.setName(sn.nextLine());
+                System.out.print("Input new color: ");
+                cur.info.setColor(sn.nextInt());
+                System.out.print("Input new price: ");
+                cur.info.setPrice(sn.nextDouble());
+                found=true;
+                break;
+            }
+            cur=cur.next;
+        }
+        if (!found) {
+            System.out.println("Find not fount any cars.");
+        }        
+    }
+
+    //Remove the first element of the list
+    public void removeFirst() {
+        if (isEmpty()) {
+            return;
+        }
+        if (head.next == null) {
+            head = tail = null;
+            return;
+        }
+        head = head.next;
+    }
+
+    //Remove the last element of the list
+    public void removeLast() {
+        if (isEmpty()) {
+            return;
+        }
+        if (head.next == null) {
+            head = tail = null;
+            return;
+        }
+        Node cur = head;
+        while (cur.next != tail) {
+            cur = cur.next;
+        }
+        tail = cur;
+        cur.next = null;
+    }
+
+    //Remove the element at position pos
+    public void removeAtPos(int pos) {
+        int size = countCars();
+        if (pos < 0 || pos >= size) {
+            return;
+        }
+        if (pos == 0) {
+            removeFirst();
+            return;
+        }
+        if (pos == size - 1) {
+            removeLast();
+            return;
+        }
+        Node cur = head;
+        int i = 0;
+        while (i + 1 != pos) {
+            cur = cur.next;
+            i++;
+        }
+        cur.next = cur.next.next;
     }
     
     //Remove car by its Id
     public void removeCarById(String xId){
-    
+        int i=0, pos=-1;
+        Node cur = head;
+        while (cur!=null){
+            if (cur.info.getId().equals(xId)){
+                pos=i; break;
+            }
+            i++; 
+            cur=cur.next;
+        }
+        if (pos!=-1)
+            removeAtPos(pos);
+        else
+            System.out.println("Find not found the cars with id as "+xId);
     }
     
     //Search cars with their price in range [firstPrice, secondPrice]
@@ -82,5 +200,6 @@ public class SinglyLinkedListObjMng {
             System.out.println(cur.info.toString());
             cur=cur.next;
         }
+        System.out.println("");
     }
 }
